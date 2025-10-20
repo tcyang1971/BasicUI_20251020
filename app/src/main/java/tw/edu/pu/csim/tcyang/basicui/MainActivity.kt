9539df09981effb534b286ce9af4676b6287b1d6
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-10-20 15:35:55 +0800

    v.3按鈕讓文字切換及彈出短訊息

--- app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.csim.tcyang.basicui
 
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,13 +18,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
@@ -60,6 +67,11 @@ fun Main(modifier: Modifier = Modifier) {
 
     var AnimalsName = arrayListOf("鴨子","企鵝",
         "青蛙","貓頭鷹","海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞")
+
+    var flag by remember { mutableStateOf("test") }
+
+    // 取得當前的 Context
+    val context = LocalContext.current
 
 
     Column (
@@ -125,9 +137,57 @@ fun Main(modifier: Modifier = Modifier) {
 
             }
 
-
-
         }
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+
+
+        Button(
+            onClick = {
+                if (flag == "test"){
+                    flag = "abc"
+                }
+                else{
+                    flag = "test"
+                }
+
+                Toast.makeText(
+                    context,
+                    "Compose 按鈕被點擊了！",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+
+        ) {
+            Text("按鈕測試")
+        }
+
+        Text(text = flag)
+
+        Spacer(modifier = Modifier.size(10.dp))
+        Row{
+            Button(onClick = {
+            }) {
+                Text(text = "歡迎修課")
+            }
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(onClick = {
+            }) {
+                Text(text = "展翅飛翔")
+            }
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(onClick = {
+            }) {
+                Text(text = "結束App")
+            }
+        }
+
 
 
 
